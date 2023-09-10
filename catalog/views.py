eb830beb5268c1adf8a915e95e428451d393da24
Danny-progg <122675148+Danny-progg@users.noreply.github.com>
2023-09-11 00:44:06 +0300

    commit 11 - модифицирован вывод и обработка запросов

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from pytils.translit import slugify
+
 from catalog.models import Product, Category, Post
 
 
@@ -42,22 +44,51 @@ class PostCreateView(CreateView):
     fields = ('title', 'content',)
     success_url = reverse_lazy('catalog:post_list')
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_post = form.save()
+            new_post.slug = slugify(new_post.title)
+            new_post.save()
+
+        return super().form_valid(form)
+
 
 class PostUpdateView(UpdateView):
     model = Post
     fields = ('title', 'content',)
-    success_url = reverse_lazy('catalog:post_list')
+    # success_url = reverse_lazy('catalog:post_list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_post = form.save()
+            new_post.slug = slugify(new_post.title)
+            new_post.save()
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('catalog:view', args=[self.kwargs.get('pk')])
 
 
 class PostListView(ListView):
     model = Post
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
 
 class PostDetailView(DetailView):
     model = Post
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
 
 
 class PostDeleteView(DeleteView):
     model = Post
     success_url = reverse_lazy('catalog:post_list')
-
