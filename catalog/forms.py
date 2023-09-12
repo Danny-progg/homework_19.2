9abbb40a525a9b65c8471cfa8b916dde8f73103d
Danny-progg <122675148+Danny-progg@users.noreply.github.com>
2023-09-13 01:54:33 +0300

    commit 14 - реализована работа с формами

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,9 +1,16 @@
 from django import forms
 
-from catalog.models import Product
+from catalog.models import Product, Version
 
 
-class ProductForm(forms.ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
 
     class Meta:
         model = Product
@@ -11,12 +18,21 @@ class ProductForm(forms.ModelForm):
         fields = ('name', 'description', 'price', 'image', 'category',)
         # exclude = ('date_start',)
 
-    def clean_name(self):
-        cleaned_data = self.cleaned_data['name']
+    # def clean_name(self):
+    #     cleaned_data = self.cleaned_data['name']
+    #
+    #     if 'казино' or 'криптовалюта' or 'крипта' or 'биржа' or 'дешево' or 'бесплатно' or 'обман' or 'полиция' or 'радар' in cleaned_data:
+    #         raise forms.ValidationError('Такое название нельзя использовать!')
+    #
+    #     return cleaned_data
 
-        if 'казино' or 'криптовалюта' or 'крипта' or 'биржа' or 'дешево' or 'бесплатно' or 'обман' or 'полиция' or 'радар' in cleaned_data:
-            raise forms.ValidationError('Такое название нельзя использовать!')
 
-        return cleaned_data
+class VersionForm(StyleFormMixin, forms.ModelForm):
+
+    class Meta:
+        model = Version
+        # fields = '__all__'
+        fields = ('product', 'version_name', 'version_sign', 'version_number',)
+        # exclude = ('date_start',)
 
 
