9eb0b7e921d37553ca7ca615242d0ae4d30dc8df
Danny-progg <122675148+Danny-progg@users.noreply.github.com>
2023-09-24 19:51:40 +0300

    commit 17 - Создано новое приложение для работы с пользователем. Определена собственная форма для пользователя, при этом задана электронная почта как поле для авторизации. Также добавлены поля телефон, страна, аватар.

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
-NUllABLE = {'blank': True, 'null': True}
+NULLABLE = {'blank': True, 'null': True}
 
 # Create your models here.
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100, verbose_name='Заголовок', **NUllABLE)
-    slug = models.CharField(max_length=100, verbose_name='slug', **NUllABLE),
-    content = models.TextField(max_length=100, verbose_name='Содержимое', **NUllABLE)
-    preview = models.ImageField(upload_to='images/', verbose_name='Изображение', **NUllABLE)
-    date_start = models.DateTimeField(verbose_name='Дата создания', **NUllABLE)
+    title = models.CharField(max_length=100, verbose_name='Заголовок', **NULLABLE)
+    slug = models.CharField(max_length=100, verbose_name='slug', **NULLABLE),
+    content = models.TextField(max_length=100, verbose_name='Содержимое', **NULLABLE)
+    preview = models.ImageField(upload_to='images/', verbose_name='Изображение', **NULLABLE)
+    date_start = models.DateTimeField(verbose_name='Дата создания', **NULLABLE)
     is_published = models.BooleanField(default=True, verbose_name='опубликовано')
     views_count = models.IntegerField(default=0, verbose_name='просмотры')
 
--- catalog/models.py
+++ catalog/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-NUllABLE = {'blank': True, 'null': True}
+NULLABLE = {'blank': True, 'null': True}
 
 
 class Category(models.Model):
     objects = None
-    name = models.CharField(max_length=100, verbose_name='Наименование', **NUllABLE)
-    description = models.CharField(max_length=100, verbose_name='Описание', **NUllABLE)
+    name = models.CharField(max_length=100, verbose_name='Наименование', **NULLABLE)
+    description = models.CharField(max_length=100, verbose_name='Описание', **NULLABLE)
 
     def __str__(self):
         return f'{self.name} - {self.description}'
@@ -17,13 +17,13 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Наименование', **NUllABLE)
-    description = models.TextField(max_length=100, verbose_name='Описание', **NUllABLE)
-    image = models.ImageField(upload_to='images/', verbose_name='Изображение', **NUllABLE)
+    name = models.CharField(max_length=100, verbose_name='Наименование', **NULLABLE)
+    description = models.TextField(max_length=100, verbose_name='Описание', **NULLABLE)
+    image = models.ImageField(upload_to='images/', verbose_name='Изображение', **NULLABLE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     price = models.IntegerField(verbose_name="Цена")
-    date_start = models.DateTimeField(verbose_name='Дата создания', **NUllABLE)
-    data_end = models.DateTimeField(verbose_name="Дата закрытия", **NUllABLE)
+    date_start = models.DateTimeField(verbose_name='Дата создания', **NULLABLE)
+    data_end = models.DateTimeField(verbose_name="Дата закрытия", **NULLABLE)
 
     def __str__(self):
         return f'{self.name} - {self.description}'
@@ -37,7 +37,7 @@ class Version(models.Model):
     objects = None
     product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Продукт')
     version_number = models.IntegerField(verbose_name='Номер версии')
-    version_name = models.CharField(max_length=100, verbose_name='Название версии', **NUllABLE)
+    version_name = models.CharField(max_length=100, verbose_name='Название версии', **NULLABLE)
     version_sign = models.BooleanField(default=True, verbose_name='Признак текущей версии')
 
     def __str__(self):
