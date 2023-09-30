7588a1e2f993d5901dfdcd3e4efce3b69e1c5812
Danny-progg <122675148+Danny-progg@users.noreply.github.com>
2023-09-30 20:23:40 +0300

    commit 19 - задача 22.2 - выполнена задача ссылки на почту

--- users/models.py
+++ users/models.py
@@ -10,6 +10,7 @@ class User(AbstractUser):
     phone = models.CharField(max_length=25, verbose_name='телефон', **NULLABLE)
     country = models.CharField(max_length=50, verbose_name='страна', **NULLABLE)
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    vrf_token = models.CharField(max_length=25, verbose_name='токен', **NULLABLE)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
--- users/views.py
+++ users/views.py
@@ -1,12 +1,13 @@
 import random
 
 from django.conf import settings
+from django.contrib import messages
 from django.contrib.auth.views import LoginView as BaseLoginView
 from django.contrib.auth.views import LogoutView as BaseLogoutView
 from django.core.mail import send_mail
 from django.shortcuts import redirect
 from django.urls import reverse_lazy, reverse
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, View
 
 from users.forms import UserRegisterForm, UserForm
 from users.models import User
@@ -27,14 +28,32 @@ class RegisterView(CreateView):
     template_name = 'users/register.html'
 
     def form_valid(self, form):
-        new_user = form.save()
+        user = form.save(commit=False)
+        user.is_active = False
+        vrf_token = ''.join([str(random.randint(0, 9)) for _ in range(12)])
+        confirm_url = self.request.build_absolute_uri(reverse('users:confirm', args=[vrf_token]))
+        user.vrf_token = vrf_token
+        user.save()
         send_mail(
             subject='Поздравляем с регистрацией!',
             message='Вы зарегистрировались на нашей платформе, добро пожаловать!',
             from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[new_user.email]
+            recipient_list=[user.email]
         )
         return super().form_valid(form)
+
+
+class ConfirmRegistrationView(View):
+    def get(self, request, vrf_token):
+        try:
+            user = User.objects.get(vrf_token=vrf_token)
+            user.is_active = True
+            user.vrf_token = None
+            user.save()
+        except User.DoesNotExist:
+            messages.error(request, "Ошибка: пользователь с указанным токеном не найден. "
+                                    "Пройдите регистрацию снова")
+        return redirect('users:register')
 
 
 class UserUpdateView(UpdateView):
